561322756151f2907add6a5a17433901f31acac7
Lundy Orlando <simplyeazy@gmail.com>
2018-09-10 10:25:27 +0700

    Release 0

--- calculator.js
+++ calculator.js
@@ -1,22 +1,35 @@
-'use strict'
+"use strict";
+
+const PI = Math.PI;
 
 class Calculator {
-  //write your code here
-  constructor () {
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
   }
-  add () {
+
+  sum() {
+    return this.num1 + this.num2;
   }
-  subtract () {
+  substract() {
+    return this.num1 - this.num2;
   }
-  multiply () {
+  multiply() {
+    return this.num1 * this.num2;
   }
-  divide () {
+  divide() {
+    return this.num1 / this.num2;
   }
-  square () {
+  power() {
+    return this.num1 ^ this.num2;
   }
-  squareRoot () {
+  sqrt() {
+    return Math.sqrt(this.num1);
   }
 }
+
+let hitung = new Calculator(5, 2);
+console.log(hitung.sum().sum(2));
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
